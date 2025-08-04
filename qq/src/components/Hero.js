652c97205ac37f1e26046f03c158150258de539b
Azthurano1 <lexa.gusew2015@yandex.ru>
2025-08-04 05:28:49 +0300

    eee\

--- qq/src/components/Hero.js
+++ qq/src/components/Hero.js
@@ -18,8 +18,60 @@ export class Hero extends Component {
                 </div>
             </div>
             <div className='sysback'>
-              {/* <img className='imgsysback' src="../images/sysback.jpg" alt='imgsysback'></img> */}
+              {/* <img className='imgsysback' src="../sysback.svg" alt='imgsysback'></img> */}
+              <div className='blockcont'>
+                <div className='promo'>
+                  <div className='promo_search'>
+                    <div className='promo_text'>https://promo.png</div>
+                    <div className='button_nav'>
+                      <img className = "vector" src="../images/Vector.svg" alt='buttonnav'></img>
+                      <img className = "qube" src='../images/qube.svg' alt='buttonnav'></img>
+                      <img className = "x" src='../images/x.svg' alt='buttonnav'></img>
+                    </div>
+                  </div>
+                  <div className='content_promo'>
+                      <div className='content_bc'>
+                      <div className='content_block_promo1'>
+                        <div className='content_text1'>
+                          <div className='H1content_promo1'>Lite</div>
+                          <div className='p1content_promo1'>Базовый минимум</div>
+                          <div className='strelka1'>{'>'}</div>
+                        </div>
+                      </div>
+                      <div className='content_block_promo1'>
+                        <div className='content_text1'>
+                          <div className='H1content_promo1'>4.0</div>
+                          <div className='p1content_promo1'>Базовый минимум</div>
+                          <div className='strelka1'>{'>'}</div>
+                        </div>
+                      </div>
+                      <div className='content_block_promo1'>
+                        <div className='content_text1'>
+                          <div className='H1content_promo1'>5.0</div>
+                          <div className='p1content_promo1'>Роскошный минимум</div>
+                          <div className='strelka1'>{'>'}</div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className='img1_content'>
+
+                </div>
+                <div classname='promo2'>
+                  <div classname ='promo_search2'>
+                    <div className='promo_text2'>https://promo.png</div>
+                    <div className='button_nav'>
+                        <img className = "vector" src="../images/Vector.svg" alt='buttonnav'></img>
+                        <img className = "qube" src='../images/qube.svg' alt='buttonnav'></img>
+                        <img className = "x" src='../images/x.svg' alt='buttonnav'></img>
+
+                    </div>
+                  </div>
+                </div>
             </div>
+            </div>
+            
         </div>
     </div>
     )
